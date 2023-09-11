052417552806e19ad94f8b32acbfd9e4edac9870
JavierIbanezB <Javier.Ibanez@ey.co.com>
2023-09-11 14:50:07 -0500

    update ask page with order ok and colors on nav bar

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,15 @@
 var boton = document.getElementById("btn-ask");
 boton.addEventListener("click", async function () {
   var container = document.getElementById("loaderContainer");
+  var askrender = document.getElementById("askrender");
   container.style.display = "block";
   var question = document.getElementById("questionModel");
   const gpt = {
     question: question.value,
   };
   document.getElementById("questionModel").textContent = "";
-  await fetch("https://gptaudit.azurewebsites.net/gpt", {
+  //https://gptaudit.azurewebsites.net/gpt for azure development - http://localhost:5000/gpt for local development
+  await fetch("http://localhost:5000/gpt", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -16,11 +18,13 @@ boton.addEventListener("click", async function () {
   })
     .then((response) => response.json())
     .then((data) => {
+      askrender.innerHTML =
+        '<h5 for="askModel">Respuesta del modelo GPT:</h5> <textarea class="form-control" disabled="true" id="askModel" rows="3"></textarea>';
       document.getElementById("askModel").textContent = data.answer;
     })
     .catch((error) => console.log(error));
   var pre_q = document.getElementById("pre-question");
-  pre_q.textContent = "Pregunta previa: " + question.value;
+  pre_q.innerHTML = `<h5 style="display: inline">Pregunta realizada: </h5><p style="display: inline">${question.value}</p>`;
   const btnAsk = document.getElementById("btn-ask");
   btnAsk.disabled = true;
   question.value = "";
